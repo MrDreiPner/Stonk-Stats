b6269c76dc2687c8103eba120e4bf0fe56a59d46
M.Porubsky <porubsky1995@hotmail.com>
2022-03-15 14:19:07 +0100

    First step

--- Stonks_Algos/main.cpp
+++ Stonks_Algos/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <assert.h>
+#include <fstream>
 
-
-using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    ifstream ip("https://de.finance.yahoo.com/quote/MSFT/history?period1=1644883200&period2=1647302400&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true&output=csv");
+
+    if(!msa.is_open()) std::cout << "Fucky Wucky" << std::endl;
+
+    string Date;
+    string Open;
+    string High;
+    string Low;
+    string Close;
+
+
     return 0;
 }
