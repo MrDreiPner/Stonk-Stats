d056d4d49287f076c01dcc1edd3545d340d37343
M.Porubsky <porubsky1995@hotmail.com>
2022-03-21 20:18:46 +0100

    Search and import work
    search gibt den letzten eintrag wieder, und import die ganze liste

--- Stonks_Algos/main.cpp
+++ Stonks_Algos/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <assert.h>
 #include <fstream>
+#include <conio.h>
 #include "aktie.h"
 
 using namespace std;
@@ -9,17 +10,30 @@ void getLine(ifstream& bigStonk, int var);
 int64_t poti(int n);
 int64_t Hash(string name);
 
+void printLastCSV(ifstream& bigStonk, Aktie hashTabelle){
+    if(!bigStonk.is_open()) cout << "Fucky Wucky" << endl;
+    cout << "Aktienname: " << hashTabelle.getAktienName() << endl;
+    cout << "Aktienkuerzel: " << hashTabelle.getAktienKuerzel() << endl;
+    cout << "WKN: " << hashTabelle.getWKN() << endl;
+    cout << "Hashwert: " << hashTabelle.getHashWert() << endl;
+    getLine(bigStonk, 0);
+    getLine(bigStonk, 2);
+    cout << "#____________________________________________________________________________________#" << endl;
+}
+
 int main(){
 
     Aktie hashTabelle[2011];
     string selAktKuerzel = "0";
     string csvCall = "../CSV/"+selAktKuerzel+".csv";
+    int64_t hashWert = 0;
     char input = '\0';
     while(input != 'q'){
+        system("CLS");
         if(selAktKuerzel != "0"){
-            cout << "\nCurrently selected Stock => " << selAktKuerzel << endl;
+            cout << "Aktuell hinterlegte Aktie => " << hashTabelle[hashWert].getAktienName() << endl;
         }
-        cout << "What do you want to do?\nADD (a), DEL (d), IMPORT (i), SEARCH (s), PLOT (p), SAVE (c), LOAD (v), QUIT (q): " << endl;
+        cout << "Was wollen Sie machen?\nADD (a), DEL (d), IMPORT (i), SEARCH (s), PLOT (p), SAVE (c), LOAD (v), QUIT (q): " << endl;
         cin >> input;
         switch(input){
             case 'a':{
@@ -32,7 +46,7 @@ int main(){
                 string WKN;
                 cout << "WKN: " << endl;
                 cin >> WKN;
-                int64_t hashWert = Hash(aktienKuerzel);
+                hashWert = Hash(aktienKuerzel);
                 int64_t emptyCheck = 0;
                 while(hashTabelle[hashWert].getHashWert() != emptyCheck){
                     hashWert = (hashWert * hashWert) % 2011;
@@ -48,6 +62,25 @@ int main(){
             }
             break;
             case 'i':{
+                if(selAktKuerzel == "="){
+                    cout << "Keine Aktie ausgewaehlt. Bitte mit SEARCH eine Aktie zum importieren aussuchen!" << endl;
+                }
+                else{
+                    ifstream bigStonk(csvCall);
+                    if(selAktKuerzel != "0"){
+                        if(!bigStonk.is_open()) cout << "Fucky Wucky" << endl;
+                        getLine(bigStonk, 0);
+                        while(bigStonk.good()){
+                            getLine(bigStonk, 1);
+                    }
+                    cout << "#____________________________________________________________________________________#" << endl;
+                    bigStonk.close();
+                    /*cout << "\n Hash test: \n" << endl;
+                    int64_t hashed = Hash(selAktKuerzel);
+                    cout << selAktKuerzel << endl;
+                    cout << hashed << endl;*/
+                    }
+                }
 
             }
             break;
@@ -57,41 +90,32 @@ int main(){
                 cout << "AktienName nach dem Sie suchen: " << endl;
                 string selAktName;
                 cin >> selAktName;
-                int64_t hashWert = Hash(selAktKuerzel);
+                hashWert = Hash(selAktKuerzel);
                 if(hashTabelle[hashWert].getHashWert() == 0){
                     selAktKuerzel = "0";
-                    cout << "Stock not found. Please add it before searching for it!" << endl;
+                    cout << "Aktie nicht gefunden!" << endl;
                 }
                 else if(hashTabelle[hashWert].getAktienName() != selAktName){
                     while(hashTabelle[hashWert].getAktienName() != selAktName){
                         hashWert = (hashWert * hashWert) % 2011;
                         if(hashTabelle[hashWert].getHashWert() == 0){
                             selAktKuerzel = "0";
-                            cout << "Stock not found at new place. Please add it before searching for it!" << endl;
+                            cout << "Aktie nicht gefunden! Tiefe Suche." << endl;
+                            break;
                         }
                     }
                     if(selAktKuerzel != "0"){
                         csvCall = "../CSV/"+selAktKuerzel+".csv";
                         ifstream bigStonk(csvCall);
-                        if(!bigStonk.is_open()) cout << "Fucky Wucky" << endl;
-                        cout << hashTabelle[hashWert].getAktienName() << endl;
-                        cout << hashTabelle[hashWert].getAktienKuerzel() << endl;
-                        cout << hashTabelle[hashWert].getWKN() << endl;
-                        cout << hashTabelle[hashWert].getHashWert() << endl;
-                        getLine(bigStonk, 0);
-                        getLine(bigStonk, 1);
+                        printLastCSV(bigStonk, hashTabelle[hashWert]);
+                        bigStonk.close();
                     }
                 }
                 else{
                     csvCall = "../CSV/"+selAktKuerzel+".csv";
                     ifstream bigStonk(csvCall);
-                    if(!bigStonk.is_open()) cout << "Fucky Wucky" << endl;
-                    cout << hashTabelle[hashWert].getAktienName() << endl;
-                    cout << hashTabelle[hashWert].getAktienKuerzel() << endl;
-                    cout << hashTabelle[hashWert].getWKN() << endl;
-                    cout << hashTabelle[hashWert].getHashWert() << endl;
-                    getLine(bigStonk, 0);
-                    getLine(bigStonk, 1);
+                    printLastCSV(bigStonk, hashTabelle[hashWert]);
+                    bigStonk.close();
                 }
             }
             break;
@@ -111,24 +135,33 @@ int main(){
             break;
             default: cout << "\nInvalid Input!" << endl;
         }
+        cout << "To continue press any button" << endl;
+        getch();
     }
-
-    ifstream bigStonk(csvCall);
-    if(!bigStonk.is_open()) cout << "Fucky Wucky" << endl;
-    getLine(bigStonk, 0);
-    while(bigStonk.good()){
-        getLine(bigStonk, 1);
-    }
-    cout << "#____________________________________________________________________________________#" << endl;
-    string hashi = "MSFT";
-    cout << "\n Hash test: \n" << endl;
-    int64_t hashed = Hash(hashi);
-    cout << hashed << endl;
     return 0;
 }
 
 
 void getLine(ifstream& bigStonk, int var){
+    if(var == 2){                                       //Looks for the last line when called in a search
+        bigStonk.seekg(-1,ios_base::end);
+        int loop = 1;
+        while(loop) {
+            char ch;
+            bigStonk.get(ch);                            // Get current byte's data
+
+            if((int)bigStonk.tellg() <= 1){             // If the data was at or before the 0th byte
+                bigStonk.seekg(0);                       // The first line is the last line
+                loop = 0;                               // So stop there
+            }
+            else if(ch == '\n') {                       // If the data was a newline
+                loop = 0;                               // Stop at the current position.
+            }
+            else {                                      // If the data was neither a newline nor at the 0 byte
+                bigStonk.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+            }
+        }
+    }
     string date;
     string open;
     string high;
@@ -144,9 +177,9 @@ void getLine(ifstream& bigStonk, int var){
     getline(bigStonk, adjClose, ',');
     getline(bigStonk, volume, '\n');
     if(var == 1)
-        cout << date << " | " << open << " | " << high << " | " << low << " | " << close << " | " << adjClose << " | " << volume << "\n";
-    else
         cout << "   " << date << "    |    " << open << "    |    " << high << "    |     " << low << "    |    " << close << "   |  " << adjClose << " |  " << volume << "\n";
+    else
+        cout << date << " | " << open << " | " << high << " | " << low << " | " << close << " | " << adjClose << " | " << volume << "\n";
 }
 
 int64_t poti(int n){
